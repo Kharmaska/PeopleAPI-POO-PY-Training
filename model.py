0f95c1e85be527509a1769de6df1862ea81b9b66
Vincent Moulin - Kharmaska <kharmachaitea@gmail.com>
2020-05-24 14:36:29 +0200

    for loop on the 100k agents agreeableness stat

--- model.py
+++ model.py
@@ -1,8 +1,15 @@
+import json
+
+
 class Agent:
-  def say_hello(self, first_name):
-    return "Bien le bonjour " + first_name + " !"
-  def __init__(self, agreeableness):
-    self.agreeableness = agreeableness
   
-agent = Agent(4)
-print(agent.agreeableness)
+  def __init__(self, **agent_attributes):
+      for attr_name, attr_value in agent_attributes.items():
+          setattr(self, attr_name, attr_value)
+
+def main():
+    for agent_attributes in json.load(open("agents-100k.json")):
+        agent = Agent(**agent_attributes)
+        print(agent.agreeableness)
+
+main()
